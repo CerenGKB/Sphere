561d2253b4994a642e18da07de281cb1c093edf7
CerenGKB <cerengokbulut@hotmail.com>
2023-09-03 20:16:35 +0300

    Created a scene with three js without errors

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
-import './style.css'
+import * as THREE from 'three'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-   
-  </div>
-`
 
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(
+  75,
+  innerWidth / innerHeight,
+  0.1,
+  1000
+)
+
+const renderer = new THREE.WebGLRenderer()
+
+renderer.setSize(innerWidth,innerHeight)
+
+document.body.appendChild(renderer.domElement)
+
+const sphere = new THREE.Mesh(new THREE.SphereGeometry(5,50,50), 
+new THREE.MeshBasicMaterial({
+  color: 0xFF000
+}))
+
+scene.add(sphere)
+
+camera.position.z = 10
+
+function animate(){
+  requestAnimationFrame(animate)
+  renderer.render(scene,camera)
+}
+
+animate()
